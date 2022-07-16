93d552f2b812376a77256bcab75532e4aafe855d
ANUSHKA-BINPLUS <anushka.tyagi@binplus.in>
2022-07-16 19:09:33 +0530

    Status module

--- src/components/Workspace/parts/Cells.jsx
+++ src/components/Workspace/parts/Cells.jsx
@@ -1,24 +1,83 @@
-import { React,  } from "react";
+import { React } from "react";
 import { Form } from "react-bootstrap";
 
 function Cells() {
-   
-      
-       return (
+  return (
     <>
+      <Form.Control
+        type="text"
+        placeholder="New Cell"
+        id="textfield1"
+        defaultValue="New Cell"
+      />
+      <Form.Control
+        type="number"
+        placeholder="12345"
+        id="textfield2"
+        defaultValue="12345"
+      />
     
+     
+        <div className="dropdown">
+          <a
+            className="btn btn-secondary w-100 "
+            href="#"
+            role="button"
+            id="dropdownMenuLink"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+          >
+            {" "}
+          </a>
+          <ul className="dropdown-menu" aria-labelledby="dropdownMenuLink">
+            <li>
+              <a className="dropdown-item bg-green text-white" href="#">
+             DONE
+              </a>
+            </li>
+            <li>
+              <a className="dropdown-item bg-orange text-white" href="#">
+              WORKING ON IT
+              </a>
+            </li>
+            <li>
+              <a className="dropdown-item bg-red text-white" href="#">
+               STUCK
+              </a>
+            </li>
+            <li>
+           
+            </li>
+            <li>
+              <a className="dropdown-item bg-black text-white" href="#">
+              NO GO
+              </a>
+            </li>
 
-    <Form.Control type="text" placeholder="New Cell" id="textfield1" defaultValue="New Cell" />
-    <Form.Control type="number" placeholder="12345" id="textfield2" defaultValue="12345" />
-    <Form.Control type="text" placeholder="Text" id="textfield3" defaultValue="Status" />
-    <Form.Control type="Date" placeholder="Date" id="textfield4" defaultValue="" />
-             
-          
+            <hr className="w-100"/>
 
-          
-   
+            <center>Edit Labels</center>
+           
+          </ul>
+        </div>
 
+        <Form>
+          <Form.Select aria-label="" classname="bg-secondary">
+          <option className="mt-2 bg-gray text-white"> </option>
+            <option className="mt-2 bg-green text-white"> DONE</option>
+            <option className="mt-2 bg-orange text-white"> WORKING ON IT</option>
+            <option className="mt-2 bg-red text-white"> STUCK</option>
+            <option className="mt-2 bg-black text-white"> NO GO</option>
+          </Form.Select>
+        </Form>
+      
+      <Form.Control
+        type="Date"
+        placeholder="Date"
+        id="textfield4"
+        defaultValue=""
+      />
     </>
-       );
-    }
-    export default Cells;
+  );
+}
+export default Cells;
--- src/components/Workspace/parts/Newfolder.jsx
+++ src/components/Workspace/parts/Newfolder.jsx
@@ -12,9 +12,11 @@ function Newfolder() {
       console.log("totally custom!")
     );
 
+   {/* <FaChevronDown size='15'/> */}
     return (
+       
       <Form>
-        {/* <FaChevronDown size='15'/> */}
+      
         <Form.Control
           type="text"
           placeholder="New Folder" defaultValue="New Folder"
@@ -49,9 +51,58 @@ function Newfolder() {
                 style={{ background: "transparent", color: "black" }}
               />
             </>
+            
           </Accordion.Collapse>
         </>
       </Accordion>
+
+      {/* <div className="accordion" id="port-accordion">
+  <div className="accordion-item">
+    <h2 className="accordion-header" id="accordion-header-one">
+      <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-one" aria-expanded="false" aria-controls="collapse-one">
+      NEW FOLDER
+            </button>
+    </h2>
+      <div id="collapse-one" className="accordion-collapse collapse" aria-labelledby="accordion-header-one" data-bs-parent="#port-accordion" >
+      <div className="accordion-body" data-hotspot="1">
+        <div className="accordion" id="country-accordion-one">
+          <div className="accordion-item" id="accordion-item-canada">
+            <h2 className="accordion-header" id="accordion-header-canada">
+              <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-canada" aria-expanded="false" aria-controls="collapse-canada">Canada</button>
+            </h2>
+            <div id="collapse-canada" className="accordion-collapse collapse" aria-labelledby="accordion-header-canada" data-bs-parent="#country-accordion-one" >
+              <div className="accordion-body">
+                <p>Accordion content</p>
+              </div>
+            </div>
+          </div>
+          <div className="accordion-item" id="accordion-item-mexico">
+            <h2 className="accordion-header" id="accordion-header-mexico">
+              <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-mexico" aria-expanded="false" aria-controls="collapse-mexico">Mexico</button>
+            </h2>
+            <div id="collapse-mexico" className="accordion-collapse collapse" aria-labelledby="accordion-header-mexico" data-bs-parent="#country-accordion-one" >
+              <div className="accordion-body">
+                <p>Accordion content</p>
+              </div>
+            </div>
+          </div>
+          <div className="accordion-item" id="accordion-item-united-states-of-america">
+            <h2 className="accordion-header" id="accordion-header-united-states-of-america">
+              <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-united-states-of-america" aria-expanded="false" aria-controls="collapse-united-states-of-america">United States Of America</button>
+            </h2>
+            <div id="collapse-united-states-of-america" className="accordion-collapse collapse" aria-labelledby="accordion-header-united-states-of-america" data-bs-parent="#country-accordion-one" >
+              <div className="accordion-body">
+                <p>Accordion content</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+ 
+</div> */}
     </>
   );
 }
