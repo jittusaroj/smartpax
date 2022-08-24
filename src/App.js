8c1812ae6415c11e575a558525898760514e2076
jittusaroj <43810688+jittusaroj@users.noreply.github.com>
2022-08-25 02:11:19 +0530

    sticky test

--- src/App.js
+++ src/App.js
@@ -20,19 +20,19 @@ const App = () => {
   return (
     <div className="app1">
       {/* <CategoriesContext.Provider value={value}> */}
-       <Routes>
-            <Route path="/" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/signup" element={<SignUp />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path='/forgot_password' element={<Forgotpassword />} />
-            <Route path='/profile' element={<Profile />} />
-            <Route path='/userlist' element={<Userlist />} />
-            <Route path='/Workspace' element={<Workspace />} />
-            <Route path='/Workspace/:workspace_id' element={<Workspace />} />
-            {/* <Route path="/ticket" element={<TicketPage />} /> */}
-            {/* <Route path="/ticket/:id" element={<TicketPage editMode={true} />} /> */}
-          </Routes>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path='/forgot_password' element={<Forgotpassword />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/userlist' element={<Userlist />} />
+        <Route path='/Workspace' element={<Workspace />} />
+        <Route path='/Workspace/:workspace_id' element={<Workspace />} />
+        {/* <Route path="/ticket" element={<TicketPage />} /> */}
+        {/* <Route path="/ticket/:id" element={<TicketPage editMode={true} />} /> */}
+      </Routes>
       {/* </CategoriesContext.Provider> */}
     </div>
   )
